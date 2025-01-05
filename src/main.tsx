51071c2e9de97e8dac4133f7a3e108bd4605671e
faridmsta <faridmustafyev@gmail.com>
2025-01-05 14:44:50 +0400

    StrictMode deactivated fro debuging

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -6,9 +6,9 @@ import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  // </StrictMode>,
 )
